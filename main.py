c2bd98ed29d67b139c50ff5271cf5311cf0621c2
Mirnes Becirbegovic <mirnesbb@gmail.com>
2025-06-12 19:37:12 +0200

    feat: store messages as a list to maintain conversation history for LLM API

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
 import os
+import sys
 from google import genai
 from dotenv import load_dotenv
+from google.genai import types
+
 
 
 def main():
-
     load_dotenv()
+    
+    args = sys.argv[1:]
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+    user_prompt = " ".join(args)
+    
     api_key = os.environ.get("GEMINI_API_KEY")
-
     client = genai.Client(api_key=api_key)
+
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
 
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        contents=messages,
     )
     print("Response:", response.text)
     print("Prompt tokens:", response.usage_metadata.prompt_token_count)
     print("Response tokens:", response.usage_metadata.candidates_token_count)
+
     
 
 
