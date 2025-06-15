eeb3587069e03df3aa5ea06e020714678280ba75
Mirnes Becirbegovic <mirnesbb@gmail.com>
2025-06-16 01:00:13 +0200

    Update tests and test files for write_file function
    - Update tests.py with new write_file function tests
    - Modify lorem.txt content from write_file testing

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
 def test():
     # result = get_files_info("calculator", ".")
@@ -19,17 +20,27 @@ def test():
     # print("Result for '../' directory:")
     # print(result)
 
-    result = get_file_content('calculator', 'lorem.txt')
+    # result = get_file_content('calculator', 'lorem.txt')
+    # print(result)
+
+    # result = get_file_content("calculator", "main.py")
+    # print(result)
+
+    # result = get_file_content("calculator", "pkg/calculator.py")
+    # print(result)
+
+    # result = get_file_content("calculator", "/bin/cat")
+    # print(result)
+
+    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
     print(result)
 
-    result = get_file_content("calculator", "main.py")
+    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
     print(result)
 
-    result = get_file_content("calculator", "pkg/calculator.py")
+    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
     print(result)
 
-    result = get_file_content("calculator", "/bin/cat")
-    print(result)
 
 
 
